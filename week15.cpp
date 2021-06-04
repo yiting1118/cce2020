c410999892976f2a53b2d2b1ce7ec949ee3ad4e6
yiting1118 <79676836+yiting1118@users.noreply.github.com>
2021-06-04 11:16:47 +0800

    Update week15.cpp

--- week15.cpp
+++ week15.cpp
@@ -1,3 +1,4 @@
+##01
 ```C
 import processing.sound.*;
 SoundFile player;
@@ -14,3 +15,23 @@ void draw(){ //畫圖,每秒60次
   if(9-s%10==0)player.play();
 }
 ```
+##
+```c
+import processing.sound.*;
+SoundFile player;
+void setup(){
+  size(400,200);
+  player = new SoundFile(this,"bell.mp3");
+}
+void draw(){
+  background(#DBA6F2);
+}
+void mousePressed(){
+  if(player.isPlaying()){
+    player.stop(); 
+  }else{
+   player.play(); 
+  }
+}
+```
+##03
