c10ca5a30ca7f3bfc6a6fabb68019fe34a5561e9
yiting1118 <79676836+yiting1118@users.noreply.github.com>
2021-06-04 11:54:37 +0800

    Update week15.cpp

--- week15.cpp
+++ week15.cpp
@@ -34,4 +34,17 @@ void mousePressed(){
   }
 }
 ```
-##03
+##03(java)
+```c
+function setup(){ //設定 只做一次
+  createCanvas(400,200);
+  textSize(40); //字型大小
+}
+
+function draw(){ //畫圖,每秒60次
+  background(219,166,242);
+  let s = second(); // 秒鐘
+  text(s,100,100);
+}
+```
+
